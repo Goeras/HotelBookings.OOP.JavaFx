ddfab7787d7651e00638769d6aac9f6f55fa8d89
Markus Göras <m.goras@hotmail.com>
2023-12-20 12:57:26 +0100

    Update LocalDatePersistenceDelegate.java
    Tillagda kommentar i koden

--- src/application/LocalDatePersistenceDelegate.java
+++ src/application/LocalDatePersistenceDelegate.java
@@ -5,6 +5,7 @@ import java.beans.Expression;
 import java.beans.PersistenceDelegate;
 import java.time.LocalDate;
 
+    // Override på två metoder från klassen "PersistenceDelegate" från java.beans, som används av XMLEncoder och XMLDecoder för att hantera specialfall vid serialisering och deserialisering av objekt.
 public class LocalDatePersistenceDelegate extends PersistenceDelegate {
     
 	// Override-metod för att lösa serialiseringsprocessen med LocalDate som XMLEncoder annars inte klarar av.
@@ -14,7 +15,7 @@ public class LocalDatePersistenceDelegate extends PersistenceDelegate {
         return new Expression(oldInstance, oldInstance.getClass(), "parse", new Object[]{date.toString()}); // Parsar datumen till strängar som encodern klarar av att hantera.
     }
 
-    // Override-metod för att jämföra om det nya objektet är en deserialiserad version av det gamla objektet.
+    // Override-metod för att jämföra om det nya objektet är en deserialiserad version av det gamla objektet och returnar då true.
     @Override
     protected boolean mutatesTo(Object oldInstance, Object newInstance) {
         return oldInstance.equals(newInstance);
